f02272a84b0f525a47c1b9d414b6d4c1efb3e921
Juho Härme <juho.harme@uta.fi>
2017-05-26 21:41:35 +0300

    Add new files for servicelist and service details

---
+++ src/servicedetails.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *
+ * Tulostaa näkymän, jossa yksittäiseen messuun liittyviä vastuita voi tutkia
+ * ja muokata yksityiskohtaisemmin.
+ *
+ */
+
+
+?>
---
+++ src/servicelist.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *
+ * Tulostaa näkymän, jossa koko yhden kauden (TODO linkki) messuja voi tutkia
+ * listana. Lisäksi tässä näkymässä on mahdollista tarkastella kaikkia messuja
+ * jonkin tietyn vastuun (TODO linkki) osalta.
+ *
+ */
+
+
+?>
