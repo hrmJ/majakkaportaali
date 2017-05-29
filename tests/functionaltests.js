8d9b301a447684aa81c67ff384cc9dac5b1ef990
Juho Härme <juho.harme@uta.fi>
2017-05-29 20:23:29 +0300

    Restructure functional tests

--- tests/functionaltests.js
+++ tests/functionaltests.js
@@ -2,35 +2,35 @@ var Nightmare = require('nightmare');
 var expect = require('chai').expect; // jshint ignore:line
 var assert = require('chai').assert; 
 
-
-describe('Alkutestit', function() {
-
-    before(function() {
-        console.log("Aloitetaan testit.");
-        this.nightmare = Nightmare({show:false});
-    });
-
-    after(function() {
-        this.nightmare.end();
-        console.log("Lopetetaan...");
-      // ...
-    });
-
-    describe('Testaa, onko messulistasivu olemassa', function() {
-      it('Käyttäjä näkee sivun otsikkona: Majakkaportaali', function(done) {
-        this.timeout('10s');
-        this.nightmare
-            .goto('http://localhost/majakkaportaali/servicelist.php')
-            .evaluate(function () {
-                return document.title;
-            })
-            .then(function(title) {
-                expect(title).to.equal('Majakkaportaali');
-                done();
-            }).catch(done);
-      });
-    });
-
-
+const nightmare = new Nightmare({
+  show: false,
+  typeInterval: 20,
+  pollInterval: 50,
+  timeout: 4000,
 });
+
+describe("Messulistasivu", () => {
+  it('Käyttäjä näkee otsikkona Majakkaportaali', (done) => {
+    nightmare
+      .goto('http://localhost/majakkaportaali/servicelist.php')
+      .evaluate(function(){
+          return document.title;
+      })
+      .then((title) => {
+        assert.equal(title, "Majakkaportaali");
+        done();
+      })
+  });
+
+  it('Käyttäjä näkee taulukon sivulla', (done) => {
+    nightmare
+      .goto('http://localhost/majakkaportaali/servicelist.php')
+      .exists('table')
+      .then((table) => {
+          assert.equal(table,true);
+          done();
+      });
+  });
+});
+
 
