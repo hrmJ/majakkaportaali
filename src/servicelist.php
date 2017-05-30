3d245b046c909a69d46385b8b6257f171d60c389
Juho Härme <juho.harme@uta.fi>
2017-05-30 21:55:40 +0300

    Clean up servicelist.php

--- src/servicelist.php
+++ src/servicelist.php
@@ -19,14 +19,10 @@ $date = date('Y-m-d');
 $season = $con->Select("SELECT id, name, startdate, enddate FROM seasons WHERE startdate <=:date AND enddate >=:date ORDER BY startdate", Array("date"=>$date),"row");
 $servicedata = $con->select("SELECT servicedate, theme, id FROM services WHERE servicedate >= :startdate & servicedate <= :enddate ORDER BY servicedate", Array("startdate"=>$season["startdate"], "enddate"=>$season["enddate"]));
 
-$tablecontent = new ServiceListTable($templatepath, $servicedata);
-
 
 $tablecontent = new ServiceListTable($templatepath, $servicedata);
-
 $slist = new Template("$templatepath/servicelist.tpl");
 $slist->Set("table", $tablecontent->Output());
-
 $layout = new Template("$templatepath/layout.tpl");
 $layout->Set("title", "Majakkaportaali");
 $layout->Set("content", $slist->Output());
