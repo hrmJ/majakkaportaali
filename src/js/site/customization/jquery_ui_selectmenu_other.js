ac54e12b3b4023591ad30fb460cf85a058054afe
Juho Härme <juho.harme@uta.fi>
2017-09-30 19:30:04 +0300

    First edit of sm widget

---
+++ src/js/site/customization/jquery_ui_selectmenu_other.js
@@ -0,0 +1,156 @@
+/**
+ *
+ * Jquery ui:n selectmenu-pluginin muokkaus niin, että
+ * mahdollista valita myös tekstikenttä.
+ *
+ */
+ $.widget("custom.select_withtext", $.ui.selectmenu, 
+     { 
+         _renderItem: function( ul, item ) {
+            if(item.label=="Jokin muu"){
+                var wrapper = $("<div>").append($("<input type='text' placeholder='Jokin muu...'>"))
+                 wrapper.unbind("click");
+            }
+            else{
+                var wrapper = $("<div>").text(item.label)
+            }
+            return $("<li>").append(wrapper).appendTo(ul);
+        },
+        open: function( event ) {
+            if ( this.options.disabled ) {
+                return;
+            }
+
+            // If this is the first time the menu is being opened, render the items
+            if ( !this._rendered ) {
+                this._refreshMenu();
+            } else {
+
+                // Menu clears focus on close, reset focus to selected item
+                this._removeClass( this.menu.find( ".ui-state-active" ), null, "ui-state-active" );
+                this.menuInstance.focus( null, this._getSelectedItem() );
+            }
+
+            // If there are no options, don't open the menu
+            if ( !this.menuItems.length ) {
+                return;
+            }
+
+            this.isOpen = true;
+            this._toggleAttr();
+            this._resizeMenu();
+            this._position();
+
+            this._on( this.document, this._documentClick );
+
+            this._trigger( "open", event );
+        },
+
+	_select: function( item, event ) {
+		var oldIndex = this.element[ 0 ].selectedIndex;
+
+		// Change native select element
+		this.element[ 0 ].selectedIndex = item.index;
+		this.buttonItem.replaceWith( this.buttonItem = this._renderButtonItem( item ) );
+		this._setAria( item );
+		this._trigger( "select", event, { item: item } );
+
+		if ( item.index !== oldIndex ) {
+			this._trigger( "change", event, { item: item } );
+		}
+
+        this.close( event );
+	},
+
+	_documentClick: {
+		mousedown: function( event ) {
+            if($(event.target).find("input[type='text']").length>0 || $(event.target).attr("type")=="text"){
+                //Jos kyseessä tekstikentän sisältävä elementti
+                return;
+            }
+			if ( !this.isOpen ) {
+				return;
+			}
+
+			if ( !$( event.target ).closest( ".ui-selectmenu-menu, #" +
+					$.ui.escapeSelector( this.ids.button ) ).length ) {
+				this.close( event );
+			}
+		}
+	},
+
+
+	_drawMenu: function() {
+		var that = this;
+
+		// Create menu
+		this.menu = $( "<ul>", {
+			"aria-hidden": "true",
+			"aria-labelledby": this.ids.button,
+			id: this.ids.menu
+		} );
+
+		// Wrap menu
+		this.menuWrap = $( "<div>" ).append( this.menu );
+		this._addClass( this.menuWrap, "ui-selectmenu-menu", "ui-front" );
+		this.menuWrap.appendTo( this._appendTo() );
+
+		// Initialize menu widget
+		this.menuInstance = this.menu
+			.menu( {
+				classes: {
+					"ui-menu": "ui-corner-bottom"
+				},
+				role: "listbox",
+				select: function( event, ui ) {
+					event.preventDefault();
+
+					// Support: IE8
+					// If the item was selected via a click, the text selection
+					// will be destroyed in IE
+					that._setSelection();
+
+                    if(ui.item.data( "ui-selectmenu-item" ).label!=="Jokin muu"){
+                        that._select( ui.item.data( "ui-selectmenu-item" ), event );
+                    }
+                    else{
+                        console.log("Moikkavaan!");
+                        $(event.target).find
+                    }
+				},
+				focus: function( event, ui ) {
+					var item = ui.item.data( "ui-selectmenu-item" );
+                    console.log("focused!");
+
+					// Prevent inital focus from firing and check if its a newly focused item
+					if ( that.focusIndex != null && item.index !== that.focusIndex ) {
+						that._trigger( "focus", event, { item: item } );
+						if ( !that.isOpen ) {
+							that._select( item, event );
+						}
+					}
+					that.focusIndex = item.index;
+
+					that.button.attr( "aria-activedescendant",
+						that.menuItems.eq( item.index ).attr( "id" ) );
+				}
+			} )
+			.menu( "instance" );
+
+		// Don't close the menu on mouseleave
+		this.menuInstance._off( this.menu, "mouseleave" );
+
+		// Cancel the menu's collapseAll on document click
+		this.menuInstance._closeOnDocumentClick = function() {
+			return false;
+		};
+
+		// Selects often contain empty items, but never contain dividers
+		this.menuInstance._isDivider = function() {
+			return false;
+		};
+	},
+
+
+     }
+);
