e869aab9e7b1625e1afb1e189f3d51caacad1789
Juho Härme <juho.harme@gmail.com>
2018-08-03 17:36:07 +0300

    use servicelist as index

--- src/index.php
+++ src/index.php
@@ -1,10 +1,38 @@
 <?php
 /**
- * Koko sivuston etusivu, jonka kautta hallitaan varsinaista messulistaa
- * ja messukohtaista näkymää.
- */
+ *
+ * Yksittäistä messua koskeva informaatio: laulut, vastuunkantajat jne.
+ *
+ **/
 
+require '../vendor/autoload.php';
 
+use Medoo\Medoo;
+
+$config = parse_ini_file("../config.ini");
+$database = new Medoo([
+    'database_type' => 'mysql',
+    'database_name' => $config["dbname"],
+    'server' => 'localhost',
+    'username' => $config["un"],
+    'password' => $config["pw"],
+    'charset' => 'utf8'
+]);
+
+$m = new Mustache_Engine(array(
+    'loader' => new Mustache_Loader_FilesystemLoader(__DIR__ . '/views')
+    ));
+
+$layout = $m->loadTemplate('layout'); 
+$servicelist = $m->loadTemplate('servicelist'); 
+
+$page_content = Array(
+    "content" => $servicelist->render(),
+    "byline" => "<h2>Messut / kausi</h2>",
+    "bodyclass" => "servicelist"
+    );
+
+echo $layout->render($page_content);
 
 
 ?>
