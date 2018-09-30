06cd7423828c8464f908e57616c75ea795b1ce38
Juho Härme <juho.harme@gmail.com>
2018-09-30 13:32:51 +0300

    edited instscr

--- install_scripts/install_dependencies.sh
+++ install_scripts/install_dependencies.sh
@@ -2,14 +2,7 @@
 # Asentaa majakkaportaalin tarvitsemat riippuvuudet. Oletuksena ubuntu 18.04.
 
 # Install ngninx mysql, php, node, composer, ruby (for sass) and some additional php packages
-apt install -y nginx \
-    mysql-server-5.7 mysql-client-5.7 \
-    php7.2-fpm php7.2-mysql php7.2-mbstring php7.2-xml \
-    nodejs npm \
-    composer phpunit \
-    ruby2.5 ruby2.5-dev \
-    wget \ 
-    python3-sqlalchemy python3-pymysql
+apt install -y nginx mysql-server-5.7 mysql-client-5.7 php7.2-fpm php7.2-mysql php7.2-mbstring php7.2-xml nodejs npm composer phpunit ruby2.5 ruby2.5-dev wget python3-sqlalchemy python3-pymysql
 
 # Install sass from ruby
 gem install sass
